774e3794e26d7f0cf8535cc970029cdbe6c4e881
c1m50c <terminalityms@gmail.com>
2021-09-14 21:09:36 -0500

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+    import About from "./components/about.svelte";
+    import Skills from "./components/skills.svelte";
+    import Resume from "./components/resume.svelte";
+    import Contact from "./components/contact.svelte";
+</script>
+
+<style>
+    .header-main {
+        text-align: center;
+        display: grid;
+        align-content: center;
+    }
+
+    .navigation-bar {
+        width: 90%;
+        margin: auto;
+        background-color: rgb(33, 33, 33);
+        border-radius: 0.5rem;
+        padding: 5px 1px 5px;
+    }
+
+    button {
+        background-color: rgb(228, 144, 55);
+        font-weight: 650;
+        margin: auto;
+        border: none;
+        border-radius: 0rem;
+        width: 10%;
+    }
+
+    button:hover {
+        cursor: pointer;
+        background-color: rgb(253, 184, 111);
+    }
+</style>
+
+<main>
+    <header class="header-main">
+        <h1>Mesmerizing welcome goes here.</h1>
+        <div class="navigation-bar">
+            <button>About</button>
+            <button>Skills</button>
+            <button>Resume</button>
+            <button>Contact</button>
+        </div>
+    </header>
+    <About/>
+    <Skills/>
+    <Resume/>
+    <Contact/>
+</main>
---
+++ src/components/about.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+</script>
+
+<style>
+    .about {
+        color: rgb(50, 50, 50);
+        background-color: rgb(228, 144, 55);
+        border-radius: 0.5rem;
+        margin: auto;
+        margin-top: 3%;
+        padding: 1px 10px 1px;
+        width: 90%;
+    }
+
+    p {
+        font-weight: 525;
+        font-size: 18px;
+    }
+</style>
+
+<div class="about">
+    <h2>About Me</h2>
+    <p>Picture some words here.</p>
+</div>
