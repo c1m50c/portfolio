b222ed619474bc7d99e84b6441db8b620737b7e3
c1m50c <terminalityms@gmail.com>
2021-09-21 21:26:30 -0500

    creativity is nonexistent

--- src/App.svelte
+++ src/App.svelte
@@ -1,49 +1,60 @@
 <script lang="ts">
-    import NavigationBar from "./components/navigation_bar.svelte";
-    import About from "./components/about.svelte";
-    import Skills from "./components/skills.svelte";
-    import Resume from "./components/resume.svelte";
-    import Contact from "./components/contact.svelte";
+    import SideBar from "./components/side_bar.svelte"
 </script>
 
 
 <style>
     :global(:root) {
+        --font-size: 16px;
         --base-text-color: rgb(51, 51, 51);
-        --section-text-color: rgb(240, 240, 240);
-        --section-bg-color: rgb(74, 74, 74);
         --button-foreground-color: rgb(53, 227, 181);
         --button-background-color: rgb(48, 157, 128);
         --hover-foreground-color: rgb(242, 100, 126);
         --hover-background-color: rgb(149, 70, 85);
     }
 
-    header {
-        text-align: center;
-    }
-
-    .main-sections-container {
-        display: grid;
-        padding: 15px 0px;
-        gap: 14px;
-        grid-template-areas:
-            "about"
-            "skills"
-            "resume"
-            "contact";
+    main {
+        font-size: var(--font-size);
+        margin-left: 5rem;
+        padding: 1rem;
     }
 </style>
 
 
+<SideBar/>
 <main>
-    <header class="header-main">
-        <img src="logo.svg" alt="logo" id="logo" width=100px height=100px/>
-        <NavigationBar/>
-    </header>
+    <h2>Header</h2>
     <div class="main-sections-container">
-        <About/>
-        <Skills/>
-        <Resume/>
-        <Contact/>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, repellat repudiandae velit fugiat molestias debitis optio molestiae. Corporis nisi aperiam esse pariatur repellendus, temporibus perspiciatis amet exercitationem! Repellendus, quis ad.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, repellat repudiandae velit fugiat molestias debitis optio molestiae. Corporis nisi aperiam esse pariatur repellendus, temporibus perspiciatis amet exercitationem! Repellendus, quis ad.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, repellat repudiandae velit fugiat molestias debitis optio molestiae. Corporis nisi aperiam esse pariatur repellendus, temporibus perspiciatis amet exercitationem! Repellendus, quis ad.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, repellat repudiandae velit fugiat molestias debitis optio molestiae. Corporis nisi aperiam esse pariatur repellendus, temporibus perspiciatis amet exercitationem! Repellendus, quis ad.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, repellat repudiandae velit fugiat molestias debitis optio molestiae. Corporis nisi aperiam esse pariatur repellendus, temporibus perspiciatis amet exercitationem! Repellendus, quis ad.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, repellat repudiandae velit fugiat molestias debitis optio molestiae. Corporis nisi aperiam esse pariatur repellendus, temporibus perspiciatis amet exercitationem! Repellendus, quis ad.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, repellat repudiandae velit fugiat molestias debitis optio molestiae. Corporis nisi aperiam esse pariatur repellendus, temporibus perspiciatis amet exercitationem! Repellendus, quis ad.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, repellat repudiandae velit fugiat molestias debitis optio molestiae. Corporis nisi aperiam esse pariatur repellendus, temporibus perspiciatis amet exercitationem! Repellendus, quis ad.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, repellat repudiandae velit fugiat molestias debitis optio molestiae. Corporis nisi aperiam esse pariatur repellendus, temporibus perspiciatis amet exercitationem! Repellendus, quis ad.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, repellat repudiandae velit fugiat molestias debitis optio molestiae. Corporis nisi aperiam esse pariatur repellendus, temporibus perspiciatis amet exercitationem! Repellendus, quis ad.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, repellat repudiandae velit fugiat molestias debitis optio molestiae. Corporis nisi aperiam esse pariatur repellendus, temporibus perspiciatis amet exercitationem! Repellendus, quis ad.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, repellat repudiandae velit fugiat molestias debitis optio molestiae. Corporis nisi aperiam esse pariatur repellendus, temporibus perspiciatis amet exercitationem! Repellendus, quis ad.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, repellat repudiandae velit fugiat molestias debitis optio molestiae. Corporis nisi aperiam esse pariatur repellendus, temporibus perspiciatis amet exercitationem! Repellendus, quis ad.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, repellat repudiandae velit fugiat molestias debitis optio molestiae. Corporis nisi aperiam esse pariatur repellendus, temporibus perspiciatis amet exercitationem! Repellendus, quis ad.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, repellat repudiandae velit fugiat molestias debitis optio molestiae. Corporis nisi aperiam esse pariatur repellendus, temporibus perspiciatis amet exercitationem! Repellendus, quis ad.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, repellat repudiandae velit fugiat molestias debitis optio molestiae. Corporis nisi aperiam esse pariatur repellendus, temporibus perspiciatis amet exercitationem! Repellendus, quis ad.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, repellat repudiandae velit fugiat molestias debitis optio molestiae. Corporis nisi aperiam esse pariatur repellendus, temporibus perspiciatis amet exercitationem! Repellendus, quis ad.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, repellat repudiandae velit fugiat molestias debitis optio molestiae. Corporis nisi aperiam esse pariatur repellendus, temporibus perspiciatis amet exercitationem! Repellendus, quis ad.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, repellat repudiandae velit fugiat molestias debitis optio molestiae. Corporis nisi aperiam esse pariatur repellendus, temporibus perspiciatis amet exercitationem! Repellendus, quis ad.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, repellat repudiandae velit fugiat molestias debitis optio molestiae. Corporis nisi aperiam esse pariatur repellendus, temporibus perspiciatis amet exercitationem! Repellendus, quis ad.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, repellat repudiandae velit fugiat molestias debitis optio molestiae. Corporis nisi aperiam esse pariatur repellendus, temporibus perspiciatis amet exercitationem! Repellendus, quis ad.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, repellat repudiandae velit fugiat molestias debitis optio molestiae. Corporis nisi aperiam esse pariatur repellendus, temporibus perspiciatis amet exercitationem! Repellendus, quis ad.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, repellat repudiandae velit fugiat molestias debitis optio molestiae. Corporis nisi aperiam esse pariatur repellendus, temporibus perspiciatis amet exercitationem! Repellendus, quis ad.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, repellat repudiandae velit fugiat molestias debitis optio molestiae. Corporis nisi aperiam esse pariatur repellendus, temporibus perspiciatis amet exercitationem! Repellendus, quis ad.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, repellat repudiandae velit fugiat molestias debitis optio molestiae. Corporis nisi aperiam esse pariatur repellendus, temporibus perspiciatis amet exercitationem! Repellendus, quis ad.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, repellat repudiandae velit fugiat molestias debitis optio molestiae. Corporis nisi aperiam esse pariatur repellendus, temporibus perspiciatis amet exercitationem! Repellendus, quis ad.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, repellat repudiandae velit fugiat molestias debitis optio molestiae. Corporis nisi aperiam esse pariatur repellendus, temporibus perspiciatis amet exercitationem! Repellendus, quis ad.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, repellat repudiandae velit fugiat molestias debitis optio molestiae. Corporis nisi aperiam esse pariatur repellendus, temporibus perspiciatis amet exercitationem! Repellendus, quis ad.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, repellat repudiandae velit fugiat molestias debitis optio molestiae. Corporis nisi aperiam esse pariatur repellendus, temporibus perspiciatis amet exercitationem! Repellendus, quis ad.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, repellat repudiandae velit fugiat molestias debitis optio molestiae. Corporis nisi aperiam esse pariatur repellendus, temporibus perspiciatis amet exercitationem! Repellendus, quis ad.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, repellat repudiandae velit fugiat molestias debitis optio molestiae. Corporis nisi aperiam esse pariatur repellendus, temporibus perspiciatis amet exercitationem! Repellendus, quis ad.
     </div>
 </main>
---
+++ src/components/side_bar.svelte
@@ -0,0 +1,86 @@
+<script lang="ts">
+
+</script>
+
+
+<style>
+    :root {
+        --background-color: rgb(40, 40, 40);
+        --transition-speed: 600ms;
+    }
+
+    .side-bar {
+        width: 5rem;
+        height: 100vh;
+        position: fixed;
+        background-color: var(--background-color);
+        transition: width 200ms ease;
+    }
+
+    .side-bar-nav {
+        list-style: none;
+        padding: 0;
+        margin: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .nav-item {
+        width: 100%;
+    }
+
+    .nav-link {
+        display: flex;
+        align-items: center;
+        height: 5rem;
+    }
+
+    .nav-link img {
+        max-width: 2rem;
+        margin: 0 1.5rem;
+    }
+
+    .link-text {
+        display: none;
+        margin-left: 1rem;
+    }
+
+    .side-bar:hover {
+        width: 16rem;
+    }
+
+    .side-bar:hover .link-text {
+        display: block;
+    }
+</style>
+
+
+<nav class="side-bar">
+    <ul class="side-bar-nav">
+        <li class="nav-item">
+            <a href="/" class="nav-link">
+                <img src="side-bar-icons/info_circle.svg" alt="about"/>
+                <span class="link-text">About</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="/" class="nav-link">
+                <img src="side-bar-icons/skills.svg" alt="about"/>
+                <span class="link-text">Skills</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="/" class="nav-link">
+                <img src="side-bar-icons/resume.svg" alt="about"/>
+                <span class="link-text">Resume</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="/" class="nav-link">
+                <img src="side-bar-icons/contact.svg" alt="about"/>
+                <span class="link-text">Contact</span>
+            </a>
+        </li>
+    </ul>
+</nav>
