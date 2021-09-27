9485738cd732027855b4620d7804e4c9a5971adf
c1m50c <terminalityms@gmail.com>
2021-09-26 21:57:38 -0500

    contact, github link

--- src/components/contact.svelte
+++ src/components/contact.svelte
@@ -4,24 +4,34 @@
 
 
 <style>
+    .logo-link-container {
+        display: flex;
+        justify-content: center;
+    }
 
+    .logo {
+        fill: var(--button-foreground-color);
+        transition: all var(--base-transition-settings);
+        width: 4rem;
+        height: 4rem;
+    }
+
+    .logo:hover, .logo:focus {
+        fill: var(--hover-foreground-color);
+    }
+
+    .logo:hover {
+        cursor: pointer;
+    }
 </style>
 
 
 <div class="info-container" id="contact">
-    <h2>Contact</h2>
-    <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus odit aliquam sunt, assumenda distinctio velit sed facilis amet ipsam! Odio maiores voluptates ratione quae esse inventore cum, temporibus veniam distinctio?
-        Laudantium ad sit, at dolores adipisci asperiores quia repellendus nostrum reiciendis laboriosam accusamus, temporibus eius natus fugit sint nisi amet et quos! Nam laudantium hic quasi magnam ut amet ea.
-        Tempore voluptate omnis ipsam repellendus temporibus qui rerum, nisi et deserunt, rem eius modi adipisci placeat veritatis animi hic quasi aperiam officiis? Itaque harum eum ex dolore vitae sit aliquid.
-        Deleniti doloribus aspernatur obcaecati alias, aliquam sed accusantium dolor qui labore ipsa ullam illo modi dolorum veritatis blanditiis in, pariatur nobis nam, est nostrum sit itaque! Vitae amet eaque illum?
-        Distinctio illo error iusto corporis saepe vero beatae tenetur in officia. Mollitia eos tempore velit aliquam alias. Facilis, laboriosam reprehenderit! Minus earum officiis magnam aperiam veniam, dignissimos animi incidunt eos!
-        Fuga reprehenderit impedit assumenda deleniti vel voluptas officiis molestias suscipit aliquam eveniet dolorem consequuntur unde fugiat, temporibus incidunt sunt. Possimus beatae excepturi fugit dolore tenetur nobis voluptatibus quos quia delectus.
-        Laboriosam voluptatibus voluptatem animi fuga tenetur id, provident eaque aperiam repellendus officiis, fugit vitae. Commodi dolores laudantium pariatur accusantium recusandae aliquid ex eum voluptates unde, aspernatur, minima repudiandae, in quaerat.
-        Voluptas, earum eos, dolores saepe nulla qui quam corporis repellendus voluptates exercitationem odit in dolorem eaque sed reiciendis! Error dolor esse dolorum minus itaque corporis illum ipsa, dignissimos tempora accusamus.
-        Quidem quam obcaecati alias quae voluptatibus accusamus nemo aperiam. Numquam dolorem quas quod ipsam optio nostrum sequi natus quo nesciunt id, eos tempore vitae ex eum, aliquid neque. Aspernatur, officia?
-        Totam voluptas similique sunt iste reiciendis ad, maiores ea quasi consequatur eos inventore illo debitis tempore velit ipsum temporibus odit consectetur? Veniam tenetur, qui optio amet officia debitis obcaecati incidunt.
-        In ratione unde quis quisquam, perspiciatis distinctio voluptatibus maxime asperiores amet? Ducimus explicabo, quibusdam, laborum voluptas perferendis accusamus nobis blanditiis aperiam, voluptatum iure ea nam sunt obcaecati autem placeat accusantium.
-        Eligendi repellat excepturi recusandae doloribus nam magni earum accusamus necessitatibus. Cupiditate iusto voluptates ex architecto cum ab dolor sint, necessitatibus beatae ea tenetur id unde, exercitationem maxime commodi facilis asperiores!
-    </p>
+    <div class="logo-link-container">
+        <a href="https://github.com/c1m50c">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="logo" id="github-logo">
+                <path d="M10.9,2.1c-4.6,0.5-8.3,4.2-8.8,8.7c-0.5,4.7,2.2,8.9,6.3,10.5C8.7,21.4,9,21.2,9,20.8v-1.6c0,0-0.4,0.1-0.9,0.1 c-1.4,0-2-1.2-2.1-1.9c-0.1-0.4-0.3-0.7-0.6-1C5.1,16.3,5,16.3,5,16.2C5,16,5.3,16,5.4,16c0.6,0,1.1,0.7,1.3,1c0.5,0.8,1.1,1,1.4,1 c0.4,0,0.7-0.1,0.9-0.2c0.1-0.7,0.4-1.4,1-1.8c-2.3-0.5-4-1.8-4-4c0-1.1,0.5-2.2,1.2-3C7.1,8.8,7,8.3,7,7.6c0-0.4,0-0.9,0.2-1.3 C7.2,6.1,7.4,6,7.5,6c0,0,0.1,0,0.1,0C8.1,6.1,9.1,6.4,10,7.3C10.6,7.1,11.3,7,12,7s1.4,0.1,2,0.3c0.9-0.9,2-1.2,2.5-1.3 c0,0,0.1,0,0.1,0c0.2,0,0.3,0.1,0.4,0.3C17,6.7,17,7.2,17,7.6c0,0.8-0.1,1.2-0.2,1.4c0.7,0.8,1.2,1.8,1.2,3c0,2.2-1.7,3.5-4,4 c0.6,0.5,1,1.4,1,2.3v2.6c0,0.3,0.3,0.6,0.7,0.5c3.7-1.5,6.3-5.1,6.3-9.3C22,6.1,16.9,1.4,10.9,2.1z"/>
+            </svg>
+        </a>
+    </div>
 </div>
