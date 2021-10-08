d4cb89e181d01f11c51d88eba7e10f45aa523373
c1m50c <terminalityms@gmail.com>
2021-10-07 20:09:43 -0500

    about me

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # portfolio
  
+## Running
+To run the website run the following command in your terminal of choice and goto localhost:5000
+```bash
+$ npm run dev
+```
--- src/components/about.svelte
+++ src/components/about.svelte
@@ -8,17 +8,13 @@
 
 
 <div class="info-container" id="about">
-    <!-- Todo: Write an actual about me -->
     <h1>About Me</h1>
     <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum iste temporibus inventore, vitae omnis cumque quasi libero perferendis voluptatibus? Assumenda, vero possimus! Animi natus deserunt hic odio facilis commodi sapiente.
-        Atque labore ipsam eveniet asperiores necessitatibus, dignissimos dolor rerum sunt aut? Et, ipsa. Natus placeat libero eos nisi ullam repellendus magnam, enim possimus deserunt atque, inventore voluptatem sint maiores distinctio.
-        Voluptatum fugiat numquam ea animi quo veritatis cupiditate voluptate asperiores expedita? Repellendus reprehenderit fugit tempore repellat sunt delectus laborum possimus ratione, vel explicabo? Reprehenderit facere inventore repellat, rerum reiciendis aliquid!
-        Eveniet impedit ipsum repudiandae! Vitae quidem maxime deserunt pariatur non, aspernatur consectetur? Voluptatem tempore sequi blanditiis nulla necessitatibus distinctio, officiis nam repudiandae porro veniam! Cum recusandae cupiditate ad illo similique.
-        At amet rem modi, unde ratione ex cupiditate accusantium excepturi dolores autem delectus voluptatum quis corporis eos corrupti harum molestias velit. Atque nisi placeat modi voluptates nesciunt unde nobis temporibus!
-        Aspernatur neque quis est possimus modi accusantium itaque qui eos minus amet fugiat aliquam, esse dicta maxime, sunt aut non fugit aliquid ducimus! Quia aliquam eum fugiat sunt labore porro.
-        Facere similique dignissimos odit doloremque officiis dolor ullam nihil culpa tenetur voluptas architecto eos facilis consequuntur, nam asperiores totam? Voluptatibus vero odit dolore ad rem. Fugit excepturi molestias officia quasi!
-        Odit aliquam neque ex iusto laborum delectus. Incidunt asperiores repellendus impedit delectus neque, necessitatibus, pariatur magni debitis ullam ipsam totam! Facere esse assumenda veniam sed possimus, sunt facilis delectus accusantium?
-        Aliquam assumenda facere fugiat ipsum consectetur harum non esse quos? Quis facilis dolorum doloremque. Sequi consectetur dicta error ipsam, pariatur, doloribus quis mollitia ipsum fuga veritatis at expedita non architecto!
+        I'm a sixteen-year-old software developer based in Lincoln, Nebraska.
+        I've been programming since the age of nine and since then it has been my primary passion throughout life.
+        Pursuing software development has been a career I've always wanted to pursue.
+        I'm skilled and very comfortable with languages such as Python and JavaScript, but learning new languages and technologies has been always been something I enjoy doing to a great degree.
+        I have a hunger to push myself and learn as much as I possibly can. I am also really good at googling resources to learn from, I would consider this one of my strongest skills.
+        Learning and programming are two things I'm extremely passionate about in life, and I'm always very happy at the opportunity to work on a new project.
     </p>
 </div>
