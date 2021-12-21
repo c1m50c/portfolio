15965045841915b06459ec47cf731f6fc3f4810f
c1m50c <terminalityms@gmail.com>
2021-12-21 14:40:35 -0600

    Attempt to fix `ProjectCard` Background Image URL Linking 🔧

--- src/components/project-cards/cellular_automata_pc.svelte
+++ src/components/project-cards/cellular_automata_pc.svelte
@@ -7,7 +7,7 @@
 <ProjectCard
     title="Cellular Automata"
     description="Python project implementing Conway's Game of Life with PyGame."
-    background_url="./../projects/cellular-automata.gif"
+    background_url="../projects/cellular-automata.gif"
 >
     <div slot="logo-link-container">
         <LogoLink link="https://github.com/c1m50c/cellular-automata" icon={Icons.Github} />
