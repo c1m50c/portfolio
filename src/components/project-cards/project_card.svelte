3ac2c8abb6ce5be3cc41d31bc16c6cb4e3390592
c1m50c <terminalityms@gmail.com>
2021-12-21 12:57:46 -0600

    More `ProjectCard` Cleanup, Transfer Global `Projects` CSS -> `ProjectCard` ✔🧹

--- src/components/project-cards/project_card.svelte
+++ src/components/project-cards/project_card.svelte
@@ -16,6 +16,48 @@
 </script>
 
 
+<style>
+    .project-card {
+        min-height: 15rem;
+        text-align: center;
+        position: relative;
+        z-index: 2;
+        display: grid;
+        align-content: center;
+    }
+
+    .bg-image {
+        position: absolute;
+        height: 100%;
+        width: 100%;
+        z-index: -1;
+        box-sizing: border-box;
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: cover;
+        filter: blur(3px) brightness(0.25);
+        border-radius: var(--border-radius);
+    }
+
+    .logo-link-container {
+        display: flex;
+        justify-content: center;
+        align-content: center;
+    }
+
+    .project-card :global(.logo) {
+        width: 4rem;
+        height: 4rem;
+        fill: var(--button-foreground-color);
+        transition: fill var(--base-transition-settings);
+    }
+
+    .project-card :global(.logo:hover) {
+        fill: var(--hover-foreground-color);
+    }
+</style>
+
+
 <div class="project-card">
     <div class="bg-image" style="background-image: url({background_url})" />
     <div class="project-card-details">
--- src/components/projects.svelte
+++ src/components/projects.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-    /* TODO: Have a better way of creating project cards, maybe something like a extendable template? */
     import PortfolioPc from "./project-cards/portfolio_pc.svelte";
     import CellularAutomataPc from "./project-cards/cellular_automata_pc.svelte";
     import SortingAlgorithmVisualizerPc from "./project-cards/sorting_algorithm_visualizer_pc.svelte";
@@ -14,43 +13,6 @@
         gap: 1.5rem;
         grid-template-columns: repeat(auto-fill, minmax(1fr, 20rem));
     }
-
-    .project-card-container :global(.project-card) {
-        min-height: 15rem;
-        text-align: center;
-        position: relative;
-        z-index: 2;
-        display: grid;
-        align-content: center;
-    }
-    
-    .project-card-container :global(.bg-image) {
-        height: 100%;
-        width: 100%;
-        position: absolute;
-        z-index: -1;
-        box-sizing: border-box;
-        background-repeat: no-repeat;
-        background-position: center;
-        background-size: cover;
-        filter: blur(3px) brightness(0.25);
-        border-radius: var(--border-radius);
-    }
-
-    .project-card-container :global(.logo-link-container) {
-        display: flex;
-        justify-content: center;
-        align-content: center;
-    }
-
-    .project-card-container :global(.logo) {
-        width: 4rem;
-        height: 4rem;
-        fill: var(--button-foreground-color);
-        transition: fill var(--base-transition-settings);
-    }
-
-    .project-card-container :global(.logo:hover) { fill: var(--hover-foreground-color); }
 </style>
 
 
