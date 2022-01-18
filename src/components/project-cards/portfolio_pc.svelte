b8e1d382ab5cb1bd1019867d4b4363d1a6951e44
c1m50c <terminalityms@gmail.com>
2022-01-18 16:42:10 -0600

    Portfolio Project `status-badge` Slot 🔧

--- src/components/project-cards/portfolio_pc.svelte
+++ src/components/project-cards/portfolio_pc.svelte
@@ -9,6 +9,10 @@
     description="My personal portfolio site, the site you're seeing right now."
     background_url="./projects/portfolio.png"
 >
+    <div slot="status-badge">
+        <img src="https://img.shields.io/github/license/c1m50c/portfolio?color=blue&style=for-the-badge" alt="License" />
+        <img src="https://img.shields.io/tokei/lines/github/c1m50c/portfolio?style=for-the-badge" alt="Lines of Code" />
+    </div>
     <div slot="logo-link-container">
         <LogoLink link="https://github.com/c1m50c/portfolio" icon={Icons.Github} />
     </div>
