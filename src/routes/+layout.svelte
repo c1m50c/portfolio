306f4b8ead08d29d1d3e226f533f8e9bb8c2edc0
c1m50c <58411864+c1m50c@users.noreply.github.com>
2024-02-12 17:47:36 -0600

    Comment-out layout `header` for now

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,14 +3,14 @@
     import "../app.css";
 </script>
 
-<header class="flex w-full justify-center items-center">
+<!-- <header class="flex w-full justify-center items-center">
     <nav class="flex gap-4 text-md text-cat-subtext0">
         <a class="navigation-link" href="/#about">About</a>
         <a class="navigation-link" href="/#skills">Skills</a>
         <a class="navigation-link" href="/#projects">Projects</a>
         <a class="navigation-link" href="/#contact">Contact</a>
     </nav>
-</header>
+</header> -->
 
 <Toaster />
 
