9c7a21b418b624555109a87569e3e6a079633eb6
c1m50c <58411864+c1m50c@users.noreply.github.com>
2024-02-12 19:21:11 -0600

    Setup `deploy.yml` workflow

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,3 +1,5 @@
+/// <reference types="svelte-adapter-azure-swa" />
+
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
@@ -10,4 +12,4 @@ declare global {
     }
 }
 
-export {};
+export { };
