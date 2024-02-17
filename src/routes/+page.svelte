3e62d5346fa532428dca60a760497b9a3c91a75c
c1m50c <58411864+c1m50c@users.noreply.github.com>
2024-02-16 18:12:43 -0600

    Scale root page down on `sm` screens

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,14 +29,14 @@
     class="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center"
 >
     <header>
-        <Peresonal class="w-72 h-72 [&>path]:fill-cat-mauve" />
-        <p class="text-cat-text text-xl font-bold">portfolio.peresonal.com</p>
+        <Peresonal class="w-72 sm:w-64 h-72 sm:h-64 [&>path]:fill-cat-mauve" />
+        <p class="text-cat-text text-xl sm:text-lg font-bold">portfolio.peresonal.com</p>
     </header>
 
-    <nav class="flex gap-x-4">
-        <a class="navigation-link text-lg" href="/skills">Skills</a>
-        <a class="navigation-link text-lg" href="/projects">Projects</a>
-        <a class="navigation-link text-lg" href="/statistics">Statistics</a>
-        <a class="navigation-link text-lg" href="/contact">Contact</a>
+    <nav class="flex flex-wrap gap-x-4">
+        <a class="navigation-link text-lg sm:text-base" href="/skills">Skills</a>
+        <a class="navigation-link text-lg sm:text-base" href="/projects">Projects</a>
+        <a class="navigation-link text-lg sm:text-base" href="/statistics">Statistics</a>
+        <a class="navigation-link text-lg sm:text-base" href="/contact">Contact</a>
     </nav>
 </section>
