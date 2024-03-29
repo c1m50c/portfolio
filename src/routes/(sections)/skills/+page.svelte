641b2454e94aa8761caa2394b9cd1b6962b8a92d
c1m50c <58411864+c1m50c@users.noreply.github.com>
2024-03-29 23:47:05 +0000

    Move "Skills" page to a card-based layout

--- src/routes/(sections)/skills/+page.svelte
+++ src/routes/(sections)/skills/+page.svelte
@@ -12,21 +12,11 @@
     <h2 class="text-center text-2xl">Skills</h2>
 
     <div class="px-16">
-        <SkillSection
-            name="Languages"
-            description="Collection of programming, command and markup languages that I have experience writing in."
-            skills={LANGUAGE_SKILLS}
-        />
-
-        <SkillSection
-            name="Libraries & Frameworks"
-            description="Collection of libraries and frameworks that I have experience using."
-            skills={LIBRARY_AND_FRAMEWORK_SKILLS}
-        />
+        <SkillSection name="Languages" skills={LANGUAGE_SKILLS} />
+        <SkillSection name="Libraries & Frameworks" skills={LIBRARY_AND_FRAMEWORK_SKILLS} />
 
         <SkillSection
             name="Technologies, Tools & Services"
-            description="Collection of technologies, tools and services that I have experience working with."
             skills={TECHNOLOGY_TOOLS_AND_SERVICE_SKILLS}
         />
     </div>
