db774a0d21546375a15ea4efd022d59115c158d9
c1m50c <58411864+c1m50c@users.noreply.github.com>
2024-07-08 16:10:55 -0500

    Remove the world in order to begin again

--- README.md
+++
@@ -1,3 +0,0 @@
-# portfolio
-
-Personal portfolio site for [@c1m50c](https://github.com/c1m50c) (me). Contains information on the skills and projects that I have, among other things.
--- src/routes/(sections)/projects/+page.svelte
+++
@@ -1,8 +0,0 @@
-<h2 class="text-center text-2xl">Projects</h2>
-
-<div class="text-center">
-    <i>
-        <span class="text-cat-maroon font-bold">TODO: </span>
-        Needs to be filled out and implemented.
-    </i>
-</div>
